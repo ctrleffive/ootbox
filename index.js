7a361cab0d406d3c05f8ebdcd08f63baea88e0b4
Chandu J S <app.coder@outlook.in>
2019-02-26 23:58:11 +0530

    fetcher fixed

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const videoId = '_5bNufWmzlo'
-
-const fetchUrl = require("fetch").fetchUrl
-fetchUrl(`https://www.youtube.com/watch?v=${videoId}`, function(error, meta, body){
+const args = process.argv.slice(2)
+if (args.length && args[0] == '--id' && args[1]) {
+  const videoId = args[1]
+  const fetchUrl = require("fetch").fetchUrl
+  const youtubeLink = `https://www.youtube.com/watch?v=${videoId}`
+  console.log('please wait..')
+  fetchUrl(youtubeLink, (error, meta, body) => {
     const content = body.toString()
-    const audioArea = content.split('type=audio%2Fmp4').pop().split('index=')[0]
-    const decodedString = decodeURIComponent(audioArea)
-    const result = decodedString.replace(/&/g, '&#038;')
-    console.log(result, decodedString)
-})
+    const split1 = content.split('type=audio%2Fmp4')[1]
+    const split2 = split1.split('url=')[1]
+    const split3 = split2.split('index=')[0]
+    const decodedString = decodeURIComponent(split3)
+    const result = decodedString.split('\\u0026')[0]
+    console.log(`Audio: ${result}`)
+  })
+} else {
+  console.log("please provide '--id'")
+}
