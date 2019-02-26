5811113508e6a2729b9c0787146931f200df0954
Chandu J S <app.coder@outlook.in>
2019-02-26 20:52:21 +0530

    seconds updated

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ fetchUrl(`https://www.youtube.com/watch?v=${videoId}`, function(error, meta, bod
     const audioArea = content.split('type=audio%2Fmp4').pop().split('index=')[0]
     const decodedString = decodeURIComponent(audioArea)
     const result = decodedString.replace(/&/g, '&#038;')
-    console.log(result)
+    console.log(result, decodedString)
 })
